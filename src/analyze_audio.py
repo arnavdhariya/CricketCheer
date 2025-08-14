f4b423df8d79b197759ea3df9b2e0708aa980c7a
Arnav Dhariya <dhariyaa@uci.edu>
2025-08-14 12:11:51 -0700

    added model (basic v) and analyzing audio software

--- src/analyze_audio.py
+++ src/analyze_audio.py
@@ -2,5 +2,8 @@ import numpy as np
 import librosa
 from pathlib import Path
 
-def analyze_audio(audio_path: Path):
-    pass
+def analyze_audio(audio_path: str):
+    y, sr = librosa.load(audio_path, sr=None)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    mfcc_mean = np.mean(mfcc, axis = 1)
+    return mfcc_mean
--- src/classification_model.py
+++ src/classification_model.py
@@ -1,3 +1,28 @@
-import os
 import numpy as np
-import sklearn
+import os
+from analyze_audio import analyze_audio
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import joblib
+
+nonnp_X = []
+nonnp_y = []
+
+for filename in os.listdir("../data/goodshot"):
+    nonnp_X.append(analyze_audio("../data/goodshot/" + filename))
+    nonnp_y.append(1)
+for filename in os.listdir("../data/badshot"):
+    nonnp_X.append(analyze_audio("../data/badshot/" + filename))
+    nonnp_y.append(0)
+
+X = np.array(nonnp_X)
+y = np.array(nonnp_y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+
+
+
+
+

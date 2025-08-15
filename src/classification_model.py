e183528a87fe88cfea717de8c2f60ef1cd82ab47
Arnav Dhariya <dhariyaa@uci.edu>
2025-08-15 12:43:38 -0700

    fixed the model and added code to give insights, need to clean up the data

--- src/classification_model.py
+++ src/classification_model.py
@@ -3,7 +3,8 @@ import os
 from analyze_audio import analyze_audio
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import joblib
+
+from sklearn.metrics import accuracy_score
 
 nonnp_X = []
 nonnp_y = []
@@ -17,12 +18,32 @@ for filename in os.listdir("../data/badshot"):
 
 X = np.array(nonnp_X)
 y = np.array(nonnp_y)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
+y_pred = model.predict(X_train)
+y_prob = model.predict_proba(X_train)
 
-
-
-
-
+# Accuracy
+accuracy = accuracy_score(y_pred, y_train)
+print(f"Test Accuracy:", accuracy)
+#
+# features = analyze_audio("../data/test2.wav")
+#
+# # Ensure features are 2D (scikit-learn expects shape [n_samples, n_features])
+# features = np.array(features).reshape(1, -1)
+#
+# # Predict
+# prediction = model.predict(features)
+# print(prediction)
+# proba = model.predict_proba(features)
+# print(proba)
+# print("Detailed Report:")
+# print("Index\tPredicted\tProbability(Good Shot)\tActual\tCorrect")
+# for i, (pred, prob, actual) in enumerate(zip(y_pred, y_prob[:, 1], y_train), 1):
+#     correct = "Yes" if pred == actual else "No"
+#     label = "Good" if pred == 1 else "Bad"
+#     actual_label = "Good" if actual == 1 else "Bad"
+#     print(f"{i}\t{label}\t{prob:.2f}\t{actual_label}\t{correct}")
